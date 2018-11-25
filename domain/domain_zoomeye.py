fa7a3d5167b3091a20fabcef27ce5c5ae101fa44
KhasMek <Boushh@gmail.com>
2018-11-25 11:10:56 -0700

    domain_zoomeye: fix module, cleanup and reenable
      - fix module logic and reenable
      - clean up formatting of code
      - format output to be a bit more readable
      - give feedback if even if no results were found
      - fix mixing of tabs and spaces in favor of spaces

--- domain/domain_zoomeye.py
+++ domain/domain_zoomeye.py
@@ -8,7 +8,7 @@ import vault
 from termcolor import colored
 import time
 
-ENABLED = False
+ENABLED = True
 
 
 class style:
@@ -21,11 +21,13 @@ def get_accesstoken_zoomeye(domain):
     password = vault.get_key('zoomeyepass')
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     datalogin = '{"username": "%s","password": "%s"}' % (username, password)
-    s = requests.post("https://api.zoomeye.org/user/login", data=datalogin, headers=headers)
+    s = requests.post("https://api.zoomeye.org/user/login", data=datalogin,
+                      headers=headers)
     responsedata = json.loads(s.text)
     if "error" in responsedata and responsedata['error'] == "bad_request":
-	return False
-    access_token1 = responsedata.get('access_token', "78c4a2dd70ddeffc5fc3c0639f86245a")
+        return False
+    access_token1 = responsedata.get('access_token',
+                                     "78c4a2dd70ddeffc5fc3c0639f86245a")
     return access_token1
 
 
@@ -33,46 +35,63 @@ def search_zoomeye(domain):
     time.sleep(0.3)
     zoomeye_token = get_accesstoken_zoomeye(domain)
     if not zoomeye_token:
-	return [False, "BAD_API"]
+        return [False, "BAD_API"]
     authData = {"Authorization": "JWT " + str(zoomeye_token)}
-    req = requests.get('http://api.zoomeye.org/web/search/?query=site:%s&page=1' % domain, headers=authData)
+    req = requests.get('http://api.zoomeye.org/web/search/?query=site:%s&page=1'
+                       % domain, headers=authData)
     return True, req.text
 
 
 def banner():
-    print colored(style.BOLD + '\n---> Finding hosts from ZoomEye\n' + style.END, 'blue')
+    print(colored(style.BOLD + '\n---> Finding hosts from ZoomEye\n' +
+                  style.END, 'blue'))
 
 
 def main(domain):
     if vault.get_key('zoomeyepass') != "" and vault.get_key('zoomeyeuser') != "":
         zoomeye_results = search_zoomeye(domain)
-	if zoomeye_results[0]:
-	        return True, json.loads(zoomeye_results)
-	else:
-		return zoomeye_results
+        if len(zoomeye_results) < 2:
+            return True, json.loads(zoomeye_results)
+        else:
+            return True, json.loads(zoomeye_results[1])
     else:
         return [False, "INVALID_API"]
 
 
 def output(data, domain=""):
     if type(data) == list and data[1] == "INVALID_API":
-        print colored(
-                style.BOLD + '\n[-] ZoomEye username and password not configured. Skipping Zoomeye Search.\nPlease refer to http://datasploit.readthedocs.io/en/latest/apiGeneration/.\n' + style.END, 'red')
+        print(colored(style.BOLD + '\n[-] ZoomEye username and password not \
+                      configured. Skipping Zoomeye Search.\nPlease refer to \
+                      http://datasploit.readthedocs.io/en/latest/apiGeneration/.'
+                      + style.END, 'red'))
+        print("")
     elif type(data) == list and data[1] == "BAD_API":
-	print colored(
-                style.BOLD + '\n[-] ZoomEye API is not functional right now.\n' + style.END, 'red')
+        print(colored(style.BOLD + '\n[-] ZoomEye API is not functional right \
+                      now.\n' + style.END, 'red'))
     else:
-        if 'matches' in data.keys():
-            print len(data['matches'])
-            for x in data['matches']:
-                if x['site'].split('.')[-2] == domain.split('.')[-2]:
-                    if 'title' in x.keys():
-                        print "IP: %s\nSite: %s\nTitle: %s\nHeaders: %s\nLocation: %s\n" % (
-                            x['ip'], x['site'], x['title'], x['headers'].replace("\n\n", ""), x['geoinfo'])
-                    else:
-                        for val in x.keys():
-                            print "%s: %s" % (val, x[val])
-        print "\n-----------------------------\n"
+        data = data[1]
+        if data['available'] > 0:
+            for site in data['matches']:
+                if site['site'].split('.')[-2] == domain.split('.')[-2]:
+                    location = site['geoinfo'] if 'geoinfo' in site else ''
+                    print("Site:\n    {}".format(site['site']))
+                    print("Title:\n    {}".format(site['title']))
+                    print("IP(s):\n    {}".format('\n    '.join(map(str, site['ip']))))
+                    print("Headers:\n    {}".format(site['headers'].replace("\r\n", "\n    ").rstrip('\n    ')))
+                    print("Geographic Information:")
+                    print("    Continent: {} ({})".format(location['continent']['names']['en'],
+                                                          location['continent']['code']))
+                    print("    Country: {} ({})".format(location['country']['names']['en'],
+                                                        location['country']['code']))
+                    print("    City: {}".format(location['city']['names']['en']))
+                    print("    Latitude: {}".format(location['location']['lat']))
+                    print("    Longitude: {}".format(location['location']['lon']))
+                    print("    ISP: {} ({})".format(location['isp'],
+                                                    location['organization']))
+                    print("")
+        else:
+            print("No results found\n")
+        print("-----------------------------\n")
 
 
 if __name__ == "__main__":
